d4b37b6e4475296ea81f3d340b5ee5fba6d2f971
Luis Fernando Forero G. <foluis@hotmail.com>
2016-04-17 21:48:43 -0500

    Angular estatico

---
+++ NA.ParkingAdvice/NA.ParkingAdvice.MVP.Front/components/mvp/controller2.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('controller2', controller2);
+
+    controller2.$inject = ['$location']; 
+
+    function controller2($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'controller2';
+
+        activate();
+
+        function activate() { }
+    }
+})();
